4d8c61e57aab6039ff2c52633c85d009f7214766
Sascha Weidner <support@sioweb.de>
2017-10-30 16:30:07 +0100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import { NgModule } from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+
+Component({
+  selector: 'angular-sioweb-confirm',
+  template: `<span>Hello world</span>`
+})
+class SiowebConfirm {
+}
+
+NgModule({
+  declarations: [ SiowebConfirm ],
+  exports: [ SiowebConfirm ]
+})
+export class SiowebConfirmModule {}
